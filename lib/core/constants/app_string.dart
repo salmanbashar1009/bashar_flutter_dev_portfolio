62017f63135ed24885bad2b87e5eaca2e7dfbce0
salmanbashar1009 <salmanbashar101@gmail.com>
2026-02-25 03:22:25 +0600

    menu item added

---
+++ lib/core/constants/app_string.dart
@@ -0,0 +1,10 @@
+class AppString {
+  static const List<String> menu = [
+    'Projects',
+    'Stack',
+    'Experience',
+    'Services',
+    'Philosophy',
+    'Contact',
+  ];
+}
--- lib/features/home/presentation/screens/widgets/app_menu_bar.dart
+++ lib/features/home/presentation/screens/widgets/app_menu_bar.dart
@@ -1,3 +1,4 @@
+import 'package:bashar_flutter_dev_portfolio/core/constants/app_string.dart';
 import 'package:bashar_flutter_dev_portfolio/core/utils/utils.dart';
 import 'package:bashar_flutter_dev_portfolio/features/home/presentation/screens/widgets/gradient_border_container.dart';
 import 'package:flutter/material.dart';
@@ -7,17 +8,38 @@ class AppMenuBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return Align(
       alignment: Alignment.topCenter,
       child: Container(
         margin: const EdgeInsets.only(top: 24),
         padding: const EdgeInsets.all(16),
-        constraints: const BoxConstraints(maxWidth: 1140),
+        constraints: const BoxConstraints(maxWidth: 1140, maxHeight: 62),
         decoration: BoxDecoration(
           border: Utils.appBorder(),
           borderRadius: BorderRadius.circular(99),
         ),
-        child: Row(children: [GradientBorderContainer()]),
+        child: Row(
+          children: [
+            GradientBorderContainer(),
+            const SizedBox(width: 10),
+            Text(
+              'Habibul Bashar',
+              style: textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.w600),
+            ),
+            Spacer(),
+            Row(
+              children: List.generate(AppString.menu.length, (index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(AppString.menu[index]),
+                );
+              }),
+            ),
+            Spacer(),
+          ],
+        ),
       ),
     );
   }
